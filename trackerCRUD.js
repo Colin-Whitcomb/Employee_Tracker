740ef6f9d2928c11a9e2fcaa93e5d45c85fb41aa
Colin Whitcomb <colinsw@berkeley.edu>
2020-07-21 06:45:21 -0700

    Renamed CRUD file. Established connection with local mySQL.

--- trackerCRUD.js
+++ trackerCRUD.js
@@ -17,8 +17,8 @@ var connection = mysql.createConnection({
 });
 
 connection.connect(function(err) {
-    if (err) throw err;
+    // if (err) throw err;
     console.log("connected as id " + connection.threadId + "\n");
-    createProduct();
+    // createProduct();
 
 });
