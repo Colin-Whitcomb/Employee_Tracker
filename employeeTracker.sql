155ec405b402dd292d89b84a7ae365d8a2e60dbc
Colin Whitcomb <colinsw@berkeley.edu>
2020-07-20 21:27:40 -0700

    Added preliminary department table.

--- employeeTracker.sql
+++ employeeTracker.sql
@@ -4,14 +4,24 @@ CREATE DATABASE employee_trackerDB;
 
 USE employee_trackerDB;
 
-CREATE TABLE employee (
+CREATE TABLE employee(
     id INT NOT NULL AUTO_INCREMENT,
     first_name VARCHAR(10) NULL,
     last_name VARCHAR(10) NULL,
     role_id INTEGER(5) NULL,
     manager_id INTEGER(5) NULL,
-    PRIMARY KEY (id)
+    PRIMARY KEY(id)
 );
 
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Bob", "Smith", 1, 12);
+INSERT INTO employee(first_name, last_name, role_id, manager_id)
+VALUES("Bob", "Smith", 1, 12);
+
+
+CREATE TABLE department(
+    id INT NOT NULL AUTO_INCREMENT,
+    _name VARCHAR (10) NULL,
+    PRIMARY KEY(id)
+);
+
+INSERT INTO department (_name)
+VALUES("HR");
