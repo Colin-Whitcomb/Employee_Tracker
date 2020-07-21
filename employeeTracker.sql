04fd6afa632ad3270bfead5f836faf58223ff857
Colin Whitcomb <colinsw@berkeley.edu>
2020-07-20 21:40:15 -0700

    Added functional employee_role table.

--- employeeTracker.sql
+++ employeeTracker.sql
@@ -4,6 +4,7 @@ CREATE DATABASE employee_trackerDB;
 
 USE employee_trackerDB;
 
+-- employee Table --
 CREATE TABLE employee(
     id INT NOT NULL AUTO_INCREMENT,
     first_name VARCHAR(10) NULL,
@@ -16,7 +17,7 @@ CREATE TABLE employee(
 INSERT INTO employee(first_name, last_name, role_id, manager_id)
 VALUES("Bob", "Smith", 1, 12);
 
-
+-- Department Table --
 CREATE TABLE department(
     id INT NOT NULL AUTO_INCREMENT,
     _name VARCHAR (10) NULL,
@@ -25,3 +26,15 @@ CREATE TABLE department(
 
 INSERT INTO department (_name)
 VALUES("HR");
+
+-- employee ROLE Table --
+CREATE TABLE employee_role (
+    id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR (10) NULL,
+    salary INTEGER (20) NULL, 
+    PRIMARY KEY(id)
+);
+
+INSERT INTO employee_role (title, salary)
+VALUES("CEO", 2000000);
+
