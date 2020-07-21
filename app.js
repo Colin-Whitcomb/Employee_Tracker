6153d33120accf4ed5e7c4569713677ddd90c54b
Colin Whitcomb <colinsw@berkeley.edu>
2020-07-20 20:13:01 -0700

    Added second prompt.

--- app.js
+++ app.js
@@ -6,23 +6,51 @@ const fs = require("fs");
 // var restart = function () {
 inquirer.prompt(
         [{
-                type: "list",
-                name: "prompt",
-                message: "What ",
-                choices: [
-                    "Add Departments",
-                    "Add Roles",
-                    "Add Employees",
-                    "View Departments",
-                    "View Roles",
-                    "View Employees",
-                    "Update Employee Roles",
-                ]
-            },
-        ])
+            type: "list",
+            name: "prompt",
+            message: "What ",
+            choices: [
+                "Add Departments",
+                "Add Roles",
+                "Add Employees",
+                "View Departments",
+                "View Roles",
+                "View Employees",
+                "Update Employee Roles",
+            ]
+        }, ])
     .then(data => {
         // console.log(data);
         if (data.prompt === 'Add Departments') {
             addDepartments()
-        } else {}
+        } 
+        else if (data.prompt === 'Add Roles'){
+            addRoles();
+        }
     });
+
+var addDepartments = () => {
+    inquirer.prompt([{
+            // Name
+            type: "input",
+            message: "What's the name of the department?",
+            name: "department_name"
+        }
+    ]).then(data => {
+        // Extract Department Name 
+        console.log(data.department_name);
+    })
+}
+
+var addRoles = () => {
+    inquirer.prompt([{
+            // Name
+            type: "input",
+            message: "What's the name of the ROLE?",
+            name: "role"
+        }
+    ]).then(data => {
+        // Extract Role Name 
+        console.log(data.role);
+    })
+}
