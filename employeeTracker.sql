38378582dc10528210be50f39ba800014e510ab4
Colin Whitcomb <colinsw@berkeley.edu>
2020-07-20 21:21:31 -0700

    First draft of operational employee table.

--- employeeTracker.sql
+++ employeeTracker.sql
@@ -4,11 +4,14 @@ CREATE DATABASE employee_trackerDB;
 
 USE employee_trackerDB;
 
-CREATE Table employee (
+CREATE TABLE employee (
     id INT NOT NULL AUTO_INCREMENT,
     first_name VARCHAR(10) NULL,
     last_name VARCHAR(10) NULL,
     role_id INTEGER(5) NULL,
     manager_id INTEGER(5) NULL,
     PRIMARY KEY (id)
-)
+);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Bob", "Smith", 1, 12);
